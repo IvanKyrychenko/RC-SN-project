230d580884a3fa252f6f9d37f4b98eeb488dee05
Ivan Kyrychenko <ivan.s.kyrychenko@gmail.com>
2021-09-13 01:07:51 +0300

    Markup updates

--- rc-sn-project/src/App.js
+++ rc-sn-project/src/App.js
@@ -1,15 +1,41 @@
 import './App.scss';
-import Header from './Header'
-import Technologies from './Technologies';
-import Footer from './Footer';
 
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' />
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYtfZRhbGQtq2BapB2MXJfWIO2QriO5Wx3qQ&usqp=CAU' />
+        </div>
+        <div>My Posts
+          <div>New post
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- rc-sn-project/src/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-  return (<div>
-    it-kamasutra.com Footer
-  </div>);
-}
-
-export default Footer;
