3c3f379a02a10249e7c84afb72e8cfd199404ab2
Ivan Kyrychenko <ivan.s.kyrychenko@gmail.com>
2021-09-23 20:46:23 +0300

    Code Refactoring

--- rc-sn-project/src/Components/Dialogs/Dialogs.jsx
+++ rc-sn-project/src/Components/Dialogs/Dialogs.jsx
@@ -21,23 +21,54 @@ const MessageItem = (props) => {
 
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Viktor' },
+        { id: 6, name: 'Valera' },
+    ];
+
+    let dialogElements = [
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />,
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />,
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />,
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+    ];
+
+    let messagesData = [
+        { id: 1, message: 'Hi!' },
+        { id: 2, message: 'How r u?' },
+        { id: 3, message: 'Sup, dude?' },
+        { id: 4, message: 'Wuzzup' },
+        { id: 5, message: 'Bonjour' },
+        { id: 6, message: 'Ola!' },
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='Dimych' id='1' />
-                <DialogItem name='Andrey' id='2' />
-                <DialogItem name='Sveta' id='3' />
-                <DialogItem name='Sasha' id='4' />
-                <DialogItem name='Viktor' id='5' />
-                <DialogItem name='Valera' id='6' />
+
+
+                { dialogElements }
+
+
             </div>
             <div className={s.messages}>
-                <MessageItem message='Hi!' />
-                <MessageItem message='How r u?' />
-                <MessageItem message='Sup, dude?' />
+                <MessageItem message={messagesData[0].message} id={messagesData[0].id} />
+                <MessageItem message={messagesData[1].message} id={messagesData[1].id} />
+                <MessageItem message={messagesData[2].message} id={messagesData[2].id} />
+                <MessageItem message={messagesData[3].message} id={messagesData[3].id} />
+                <MessageItem message={messagesData[4].message} id={messagesData[4].id} />
+                <MessageItem message={messagesData[5].message} id={messagesData[5].id} />
             </div>
         </div>
     )
 }
 
 export default Dialogs;
+
--- rc-sn-project/src/Components/Profile/MyPosts/MyPosts.jsx
+++ rc-sn-project/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import s from './MyPosts.module.scss'
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hi! How are u?', likesCount: 20},
+        {id: 2, message: "It's my first post", likesCount: 20}
+    ];
+
     return (<div>
         <div className={s.postsBlock}><h3>My Posts</h3>
             <div>
@@ -13,8 +19,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="Hi! How are u?" likesCount='20' />
-                <Post message="It's my first post" likesCount='30' />
+                <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+                <Post message={postData[1].message} likesCount={postData[1].likesCount} />
             </div>
         </div>
     </div>)
