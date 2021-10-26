b123f77db5ccc5eeefe9485e32a6a0d0b49911b7
Ivan Kyrychenko <ivan.s.kyrychenko@gmail.com>
2021-10-27 02:03:04 +0300

    Minor Update

--- rc-sn-project/src/Components/Dialogs/DialogsContainer.jsx
+++ rc-sn-project/src/Components/Dialogs/DialogsContainer.jsx
@@ -1,23 +1,43 @@
 import './Dialogs.scss';
 import { updateNewMessageBodyCreator, sendMessageCreator } from '../../Redux/DialogsReducer';
 import Dialogs from './Dialogs';
+import { connect } from 'react-redux';
 
 const DialogsContainer = (props) => {
 
     let state = props.store.getState().dialogsPage;
 
-        let onSendMessageClick = () => {
-            props.store.dispatch(sendMessageCreator());
-        }
-        
-        let onNewMessageChange = (body) => {
-          props.store.dispatch(updateNewMessageBodyCreator(body));
-        }
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator());
+    }
+
+    let onNewMessageChange = (body) => {
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    }
 
     return (
-        <Dialogs updateNewMessageBody={ onNewMessageChange } sendMessage={ onSendMessageClick }
-        dialogsPage={ state }/>
+        <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick}
+            dialogsPage={state} />
     );
 }
+
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+
+let mapDispatchToProps = () => {
+    return {
+        updateNewMessageBody: () => {
+            props.store.dispatch(updateNewMessageBodyCreator(body));
+        },
+        sendMessage: () => {
+            props.store.dispatch(sendMessageCreator());
+         }
+    }
+}
+
+const SuperDialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
